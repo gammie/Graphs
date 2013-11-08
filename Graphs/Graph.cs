eeb60ec6369d0f48268a4de82f3c3cbc212a16d2
Daniel Piata <daniel.piasta@gmail.com>
2013-11-08 11:47:20 +0100

    Matrix graph in work

---
+++ Graphs/Graph.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+
+    //grafy z krawedziami wazonymi i zwyklymi
+    //grafy na matrixie i liscie
+    //
+
+     
+
+
+    class Vertex<T>
+    {
+        private int id;
+        public int Id { get { return id; } }
+
+        private T value;
+        public T Value { get { return value; } }
+        public List<Vertex<T>> neighbors;
+        public List<Vertex<T>> Neighbors
+        {
+            get
+            {
+                if (neighbors == null)
+                {
+                    neighbors = new List<Vertex<T>>();
+                    return neighbors;
+                }
+                else return neighbors;
+            }
+        }
+
+        public Vertex(int _id, T _val)
+        {
+
+            id = _id;
+            value = _val;
+        }
+
+
+    }
+
+
+    class Edge<T>
+    {
+        private Vertex<T> fromVertex;
+        public Vertex<T> FromVertex { get { return fromVertex; } }
+
+        private Vertex<T> toVertex;
+        public Vertex<T> ToVertex { get { return toVertex; } }
+
+        public Edge(Vertex<T> from, Vertex<T> to)
+        {
+            fromVertex = from;
+            toVertex = to;
+        }
+
+
+    }
+
+    class WeightedEdge<T> : Edge<T>
+    {
+        private double weight;
+        public double Weight { get { return weight; } }
+
+        public WeightedEdge(Vertex<T> from, Vertex<T> to, double _weight)
+            : base(from, to)
+        {
+            weight = _weight;
+        }
+
+
+    }
+
+    interface IGraph<T>
+    {
+         void AddEdge(Edge<T> edge);
+
+         public void RemoveEdge(Edge<T> edge);
+
+         void AddVertex(Vertex<T> vertex);
+
+         public void RemoveVertex(Vertex<T> vertex);
+         string printVertexes();
+         Vertex<T> getVertexById(int id);
+
+        // public Edge<T> getEdge(Vertex<T> from, Vertex<T> to);
+
+          
+
+
+    }
+
+
+
+    class MatrixGraph<T> : IGraph<T>
+    {
+
+        List<Vertex<T>> vertexList;
+       // List<Edge<T>> edgeList;
+        bool digraf;
+        int[,] aMatrix;
+        int highestIndex = 0;
+
+        public MatrixGraph(bool _digraph)
+        {
+            digraf = _digraph;
+            vertexList = new List<Vertex<T>>();
+            aMatrix = new int[,]{};
+
+        }
+
+        public void AddVertex(Vertex<T> _vertex)
+        {
+            //if (_vertex.Id >= highestIndex)
+            //{
+            //    highestIndex = _vertex.Id;
+            //    aMatrix = _resizeArray(aMatrix, highestIndex + 1, highestIndex + 1);
+                
+            //}
+
+            vertexList.Add(_vertex);
+        }
+
+        public void AddEdge(Edge<T> edge)
+        {
+            int from = edge.FromVertex.Id;
+            int to = edge.ToVertex.Id;
+            if (!digraf)
+            {
+
+
+                if (highestIndex <= Math.Max(to, from))
+                {
+                    highestIndex = Math.Max(to, from);
+
+                    aMatrix = _resizeArray(aMatrix, highestIndex + 1, highestIndex + 1);
+                    aMatrix[from, to] = 1;
+                }
+                else { aMatrix[from, to] = 1; }
+               
+
+
+             
+            }
+            else
+            {
+                aMatrix[to, from] = 1;
+                aMatrix[from, to] = 1;
+            }
+
+        }
+
+        public Vertex<T> getVertexById(int id)
+        {
+            return vertexList.FirstOrDefault(x => x.Id == id);
+        }
+
+        public void RemoveVertex(Vertex<T> vertex)
+        {
+            vertexList.Remove(vertex);
+        }
+
+        public void RemoveEdge(Edge<T> edge)
+        {
+            int from = edge.FromVertex.Id;
+            int to = edge.ToVertex.Id;
+
+
+        }
+
+
+        public string printVertexes()
+        {
+            string tmp="";
+            for (int i = 0; i < highestIndex; i++)
+            {
+                for (int j = 0; j < highestIndex; j++)
+                {
+                    System.Console.Write(aMatrix[i, j]);
+
+                }
+
+            }
+
+
+
+            foreach (var vertex in vertexList)
+            {
+                tmp += vertex.Id;
+            }
+            return tmp;
+        }
+
+        public override string ToString()
+        {
+            
+
+            string grapRep = "";
+
+            for (int i = 0; i < aMatrix.GetLength(1); i++)
+            {
+                for (int j = 0; j < aMatrix.GetLength(1); j++)
+                {
+
+                    int val = aMatrix[i, j];
+                    grapRep += "\t"+val;
+                }
+                grapRep += "\n";
+            }
+
+            return grapRep;
+        }
+
+
+        protected int[,] _resizeArray(int[,] original, int x, int y)
+        {
+            int[,] newArray = new int[x, y];
+            int minX = Math.Min(original.GetLength(0), newArray.GetLength(0));
+            int minY = Math.Min(original.GetLength(1), newArray.GetLength(1));
+
+            for (int i = 0; i < minY; ++i)
+                Array.Copy(original, i * original.GetLength(0), newArray, i * newArray.GetLength(0), minX);
+
+            return newArray;
+        }
+        
+
+
+
+
+
+
+
+
+    }
+}
+
+    //class ListGraph<T> : IGraph<T>{}
+
+        
+    //}
---
+++ Graphs/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Graphs
+{
+    class Program{
+    
+        static void Main(string[] args)
+        {
+
+            IGraph<int> myGraf;
+
+            myGraf = new MatrixGraph<int>(false);
+
+            myGraf.AddVertex(new Vertex<int>(0, 3));
+            myGraf.AddVertex(new Vertex<int>(1, 2));
+            myGraf.AddVertex(new Vertex<int>(2, 3));
+            myGraf.AddVertex(new Vertex<int>(3, 2));
+            myGraf.AddVertex(new Vertex<int>(4, 3));
+            
+
+
+            myGraf.printVertexes();
+
+            
+            myGraf.AddEdge(new Edge<int>(myGraf.getVertexById(0), myGraf.getVertexById(4)));
+
+            myGraf.AddEdge(new Edge<int>(myGraf.getVertexById(1), myGraf.getVertexById(3)));
+            myGraf.AddEdge(new Edge<int>(myGraf.getVertexById(0), myGraf.getVertexById(1)));
+
+            Console.WriteLine(myGraf.ToString());
+
+            int t = Console.Read();
+        }
+    }
+}
